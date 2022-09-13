49278f337af0c5bcb5561bed021e22a8ec4329f0
CanonSamson <samsoncanon2018@gmail.com>
2022-09-13 10:52:25 -0700

    update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,71 +1,53 @@
 import { HashLink } from 'react-router-hash-link';
-
+import { MdMail } from 'react-icons/md'
 
 import { FaTwitter, FaLinkedin } from "react-icons/fa";
 const Footer = () => {
     return (
         <div className=" bg-orange py-10 px-5   relative">
-
-            <div>
-                <div className=' flex justify-between'>
-                    <ul className='  '>
-                        <li className=' text-blue py-21 '>
-                            <HashLink smooth to="#features" >Quick Links</HashLink>
-                        </li>
-
-                        <li className=' text-white py-1  '>
-                            <HashLink smooth to="#about" >FAQS</HashLink>
-                        </li>
-
-                        <li className=' text-white py-1  '>
-                            <HashLink smooth to="#contactus" >Register</HashLink>
-                        </li>
-
-
+            <div className=' grid grid-cols-1  tablet:grid-cols-6 gap-4 py-5'>
+                <div className=' col-span-2 text-white'>
+                    <span className=' text-xl font-semibold '>Stay Up To date </span>
+                    <p> With Our New Products And Services</p>
+                    <div className=' flex justify-between bg-white my-3  rounded-xl overflow-hidden'>
+                        <input className=' focus:outline-none px-4' placeholder='Enter Your Email' type="text" name="" id="" />
+                        <div className=' bg-blue p-3 '>
+                            <MdMail />
+                        </div>
+                    </div>
+                </div>
+                <div className='flex col-span-4 justify-around'>
+                    <ul>
+                        <li className=' my-4 text-blue'>Quick Links</li>
+                        <li className=' my-4 text-white'>FAQS</li>
+                        <li className=' my-4 text-white'>Register</li>
                     </ul>
-
-                    <ul className='  '>
-                        <li className=' text-blue py-1 '>
-                            <HashLink smooth to="#features" >About</HashLink>
-                        </li>
-
-                        <li className=' text-white py-1  '>
-                            <HashLink smooth to="#about" >Why Us</HashLink>
-                        </li>
-
-                        <li className=' text-white py-1  '>
-                            <HashLink smooth to="#contactus" >Features</HashLink>
-                        </li>
+                    <ul>
+                        <li className=' my-4 text-blue'>About</li>
+                        <li className=' my-4 text-white'>Why Us</li>
+                        <li className=' my-4 text-white'>Features</li>
+                        <li className=' my-4 text-white'>Pricing</li>
                     </ul>
-
-                    <ul className='  '>
-                        <li className=' text-blue py-1 '>
-                            <HashLink smooth to="#features" >Legal</HashLink>
-                        </li>
-
-                        <li className=' text-white py-1  '>
-                            <HashLink smooth to="#about" >Terms </HashLink>
-                        </li>
-
-                        <li className=' text-white py-1  '>
-                            <HashLink smooth to="#contactus" >Privacy Policy</HashLink>
-                        </li>
+                    <ul>
+                        <li className=' my-4 text-blue'>Legal</li>
+                        <li className=' my-4 text-white'>Terms </li>
+                        <li className=' my-4 text-white'>Privacy Policy</li>
                     </ul>
 
                 </div>
-                {/* 
-                <div className=' flex items-center  '>
-                    <a className='text-white ' href="https://www.linkedin.com/company/eazi-thenga/">
-                        <FaLinkedin />
-                    </a>
+            </div>
 
-                    <a className='text-white mx-10'  href="https://twitter.com/EaziThenga">
-                        <FaTwitter />
-                    </a>
 
-                </div> */}
+
+            <div className=' flex flex-col items-center justify-center pt-10 border-t border-white  '>
+                <div className='flex items-center justify-center my-4 '>
+                    <a className='text-white ' href="https://www.linkedin.com/company/eazi-thenga/"> <FaLinkedin size={30} /></a>
+                    <a className='text-white mx-10' href="https://twitter.com/EaziThenga"><FaTwitter size={30} /></a>
+                </div>
+                <span className=' text-center'>  All Rights Reserved - Eazi Thenga 2022</span>
 
             </div>
+
         </div>
     );
 }
